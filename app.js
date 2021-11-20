a7a6be42de18a964e54a46dad44b1d11600c2b25
mikeorg1 <organisciak2@gmail.com>
2021-11-20 06:57:28 -0500

    added on js

--- app.js
+++ app.js
@@ -1,80 +1,83 @@
-//try to assign rock paper scissors to 1, 2, 3.
-//have the program randomly pick one of the numbers. 
-//Return the associated String.
-
 function computerPlay() {
-   computerNum = Math.floor(Math.random() * 3) + 1;
-   if (computerNum < 2) {
-       return "Rock";
-   } else if (computerNum < 3) {
-       return "Paper";
-   } else {
-       return "Scissors";
-   }
-};
-
-function playRound() {
-    let playerSelection = prompt("Rock, Paper, or Scissors?");
-    let computerSelection = computerPlay();
-
-    if (playerSelection.toUpperCase() === "ROCK") {
-        if (computerSelection == "Scissors") {
-            console.log("Computer chose: " + computerSelection);
-            return "You win!";
-        } else if (computerSelection == "Paper") {
-            console.log("Computer chose: " + computerSelection);
-            return "You lose!";
-        } else {
-            console.log("Computer chose: " + computerSelection);
-            return "Tie!";
-        }
-    } else if (playerSelection.toUpperCase() === "PAPER") {
-        if (computerSelection == "Rock") {
-            console.log("Computer chose: " + computerSelection);
-            return "You win!";
-        } else if (computerSelection == "Scissors") {
-            console.log("Computer chose: " + computerSelection);
-            return "You lose!";
-        } else {
-            console.log("Computer chose: " + computerSelection);
-            return "Tie!";
-        }
-    } else if (playerSelection.toUpperCase() === "SCISSORS") {
-        if (computerSelection == "Paper") {
-            console.log("Computer chose: " + computerSelection);
-            return "You win!";
-        } else if (computerSelection == "Rock") {
-            console.log("Computer chose: " + computerSelection);
-            return "You lose!";
-        } else {
-            console.log("Computer chose: " + computerSelection);
-            return "Tie!";
-        }
-    } else {
-        return "Invalid selection";
-    }
-    
+  computerNum = Math.floor(Math.random() * 3) + 1;
+  if (computerNum < 2) {
+    return "Rock";
+  } else if (computerNum < 3) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
 }
 
-function game() {
-    let score = 0;
-    for (var i = 1; i < 6; i++){
-        let result = playRound();
-        if (result == "You win!") {
-            console.log(result);
-            score++
-        } else if (result == "You lose!" || "Tie!") {
-            console.log(result);
-        } else {
-            console.log(result);
-        }
-        console.log("Your score is: " + score + "/5");
-    }
-    if (score > 2) {
-        console.log("You beat the computer!");
-    } else {
-        console.log("The computer beat you!");
-    }
- }
+function playRound() {
+  let computerSelection = computerPlay();
 
-console.log(game());
+  if (playerSelection.toUpperCase() === "ROCK") {
+    if (computerSelection == "Scissors") {
+      console.log("Computer chose: " + computerSelection);
+      console.log("You win!");
+    } else if (computerSelection == "Paper") {
+      console.log("Computer chose: " + computerSelection);
+      console.log("You lose!");
+    } else {
+      console.log("Computer chose: " + computerSelection);
+      console.log("Tie!");
+    }
+  } else if (playerSelection.toUpperCase() === "PAPER") {
+    if (computerSelection == "Rock") {
+      console.log("Computer chose: " + computerSelection);
+      console.log("You win!");
+    } else if (computerSelection == "Scissors") {
+      console.log("Computer chose: " + computerSelection);
+      console.log("You lose!");
+    } else {
+      console.log("Computer chose: " + computerSelection);
+      console.log("Tie!");
+    }
+  } else if (playerSelection.toUpperCase() === "SCISSORS") {
+    if (computerSelection == "Paper") {
+      console.log("Computer chose: " + computerSelection);
+      console.log("You win!");
+    } else if (computerSelection == "Rock") {
+      console.log("Computer chose: " + computerSelection);
+      console.log("You lose!");
+    } else {
+      console.log("Computer chose: " + computerSelection);
+      console.log("Tie!");
+    }
+  } else {
+    return "Invalid selection";
+  }
+}
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    playerSelection = button.id;
+    playRound();
+  });
+});
+
+// function game() {
+//     let score = 0;
+//     for (var i = 1; i < 6; i++){
+//         let result = playRound();
+//         if (result == "You win!") {
+//             console.log(result);
+//             score++
+//         } else if (result == "You lose!" || "Tie!") {
+//             console.log(result);
+//         } else {
+//             console.log(result);
+//         }
+//         console.log("Your score is: " + score + "/5");
+//     }
+//     if (score > 2) {
+//         console.log("You beat the computer!");
+//     } else {
+//         console.log("The computer beat you!");
+//     }
+//  }
+
+// console.log(game());
